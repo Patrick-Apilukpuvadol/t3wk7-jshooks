227fb821d060e4fe0b85e960593d730f2a8aa9dd
Patrick-Apilukpuvadol <chimei983@hotmail.com>
2023-09-26 19:35:45 +1000

    feat: Project Init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import PokemonHook from './PokemonHook';
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonHook />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PokemonHook.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function PokemonHook(){
+
+    return (
+        <div>
+            <h1>Feature not yet implemented.</h1>
+        </div>
+    )
+}
